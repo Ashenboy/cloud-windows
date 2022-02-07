1337303dbc16c64384507af0391dbde68e3b6e74
Ashenboy <66189779+Ashenboy@users.noreply.github.com>
2022-02-07 10:18:14 +0530

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,9 +1,8 @@
-ÿþ
-@echo off
-echo FreeRDP By NaufalCream!
-tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
+@echo off
+echo UpCrew Groub.
+tasklist | find /i "ngrok.exe" >Nul && goto check || echo "NGROK tidak bisa cuy, pastikan NGROK_AUTH_TOKEN benar di Settings > Secrets > Repository secret. atau ngrokmu sudah terkoneksi dengan yang lain coba periksa : https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
 :check
-ping 127.0.0.1 > nul
+ping 127.0.0.1 > null
 cls
-echo FreeRDP By NaufalCream!
+echo UpCrew Groub.
 goto check
